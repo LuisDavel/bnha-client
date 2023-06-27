df4492fbd622a5613683fcf95ee769111f52c848
Luis Davel <42303034+LuisDavel@users.noreply.github.com>
2023-06-27 13:28:48 -0300

    Update index.tsx

--- src/pages/patch-notes/index.tsx
+++ src/pages/patch-notes/index.tsx
@@ -1,3 +1,42 @@
 export default function PatchNotes() {
   return <>aaa</>;
 }
+
+
+import CardHeroPatchNotes from 'components/CardHeroPatchNotes';
+import { api } from 'lib/axios';
+import { useQuery } from 'react-query';
+
+import Select from 'react-select';
+
+export default function PatchNotes() {
+  const { data, error, isLoading } = useQuery(
+    'user',
+    async () => await api.get('user.php')
+  );
+  const userData = data?.data.data;
+  const option = userData?.map(v => ({
+    value: v.id,
+    label: v.name
+  }));
+
+  return (
+    <>
+      <Select options={option} />
+      {isLoading ? (
+        <p> Carregando ... </p>
+      ) : (
+        result?.map((v: any) => (
+          <>
+            <S.Text>
+              {v.name}: <span>{v.descricao}</span>
+            </S.Text>
+            <S.Hr className="hr anim" />
+          </>
+        ))
+      )}
+      <input type="text" />
+      {/* <CardHeroPatchNotes /> */}
+    </>
+  );
+}
